456925ff404a24c20d366d16d87f1513774ff478
Jackson <jrmoffet@gmail.com>
2021-11-24 15:27:48 -0700

    Fixed tetris music

--- src/sound.c
+++ src/sound.c
@@ -39,10 +39,14 @@ void init_sound_tables() {
   }
   // Precompute ARR values for each note
   for (int i = 0; i < melody1_len; i++) {
-    melody1[i] = (48000000 / (melody1[i] * SAMPLES)) - 1;
+    if (melody1[i] != 0) {
+      melody1[i] = (48000000 / (melody1[i] * SAMPLES)) - 1;
+    }
   }
   for (int i = 0; i < melody2_len; i++) {
-    melody2[i] = (48000000 / (melody2[i] * SAMPLES)) - 1;
+    if (melody2[i] != 0) {
+      melody2[i] = (48000000 / (melody2[i] * SAMPLES)) - 1;
+    }
   }
 }
 
@@ -84,31 +88,46 @@ void init_tim16() {
 void TIM16_IRQHandler() {
   // Ack the interrupt
   TIM16->SR &= ~TIM_SR_UIF;
+  TIM16->CR1 &= ~TIM_CR1_CEN;
   // Status led for lazy debug
   GPIOC->BSRR = GPIO_BSRR_BR_9 | (GPIO_BSRR_BS_9 & ~(GPIOC->ODR));
   melody_idx += 1;
-  uint16_t *mel_arr, *dur_arr;
-  if(melody_select == 0 || melody_select == 1) {
-    if (melody_idx >= melody1_len) {
-      melody_idx = 0;
-      melody_select++;
-    }
-    mel_arr = melody1;
-    dur_arr = noteDurations1;
-  }
+  uint16_t nxt_note, nxt_dur;
   if (melody_select == 2) {
     if(melody_idx >= melody2_len) {
       melody_idx = 0;
       melody_select = 0;
-      mel_arr = melody1;
-      dur_arr = noteDurations1;
+      nxt_note = melody1[melody_idx];
+      nxt_dur = noteDurations1[melody_idx];
     } else {
-      mel_arr = melody2;
-      dur_arr = noteDurations2;
+      nxt_note = melody2[melody_idx];
+      nxt_dur = noteDurations2[melody_idx];
+    }
+  } else {
+    if(melody_idx >= melody1_len) {
+      melody_idx = 0;
+      melody_select++;
+    }
+    if(melody_select == 2) {
+      nxt_note = melody2[melody_idx];
+      nxt_dur = noteDurations2[melody_idx];
+    } else {
+      nxt_note = melody1[melody_idx];
+      nxt_dur = noteDurations1[melody_idx];
     }
   }
-  TIM2->ARR = mel_arr[melody_idx];
-  TIM16->ARR = dur_arr[melody_idx];
+
+
+  // Needed to make sure we don't set the arr to zero on accident for a rest
+  // note
+  if (!nxt_note) {
+    TIM2->CR1 &= ~(TIM_CR1_CEN);
+  } else {
+    TIM2->CR1 |= TIM_CR1_CEN;
+  }
+  TIM2->ARR = nxt_note;
+  TIM16->ARR = nxt_dur;
+  TIM16->CR1 |= TIM_CR1_CEN;
 }
 
 void init_sound_dma() {
@@ -137,11 +156,11 @@ void init_dac() {
 }
 
 uint16_t melody1[] = {
-    NOTE_E5, NOTE_B4, NOTE_C5, NOTE_D5, NOTE_E5, NOTE_D5, NOTE_C5,
-    NOTE_B4, NOTE_A4, NOTE_A4, NOTE_C5, NOTE_E5, NOTE_D5, NOTE_C5,
-    NOTE_B4, NOTE_E4, NOTE_B4, NOTE_C5, NOTE_D5, NOTE_E5, NOTE_C5,
-    NOTE_A4, NOTE_A4, NOTE_A3, NOTE_B3, NOTE_C4, NOTE_D4, NOTE_D5,
-    NOTE_F5, NOTE_A5, NOTE_A5, NOTE_A5, NOTE_G5, NOTE_F5, NOTE_E5,
+    NOTE_E5, NOTE_B4, NOTE_C5, NOTE_D5, NOTE_E5, NOTE_D5, NOTE_C5, NOTE_B4,
+    NOTE_A4, NOTE_A4, NOTE_C5, NOTE_E5, NOTE_D5, NOTE_C5,
+    NOTE_B4, NOTE_E4, NOTE_B4, NOTE_C5, NOTE_D5, NOTE_E5,
+    NOTE_C5, NOTE_A4, REST, NOTE_A3, NOTE_B3, NOTE_C4,
+    NOTE_D4, NOTE_D5, NOTE_F5, NOTE_A5, NOTE_A5, NOTE_A5, NOTE_G5, NOTE_F5, NOTE_E5,
     NOTE_C5, NOTE_E5, NOTE_F5, NOTE_E5, NOTE_D5, NOTE_C5, NOTE_B4,
     NOTE_B4, NOTE_C5, NOTE_D5, NOTE_E5, NOTE_C5, NOTE_A4, NOTE_A4,
 };
@@ -165,13 +184,28 @@ const uint16_t melody1_len = sizeof(melody1) / sizeof(melody1[0]);
 const uint16_t melody2_len = sizeof(melody2) / sizeof(melody2[0]);
 
 const uint16_t noteDurations1[] = {
-    320, 160, 160, 160, 80,  80,  160, 160, 320, 160, 160, 320, 160,
-    160, 160, 160, 160, 160, 320, 320, 320, 320, 160, 160, 160, 160,
-    160, 320, 160, 160, 80,  80,  160, 160, 480, 160, 160, 80,  80,
-    160, 160, 320, 160, 160, 320, 320, 320, 320, 640};
+  QUARTER_NOTE, EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE, SIXTEENTH_NOTE,  SIXTEENTH_NOTE,  EIGHTH_NOTE, EIGHTH_NOTE,
+  QUARTER_NOTE, EIGHTH_NOTE, EIGHTH_NOTE, QUARTER_NOTE, EIGHTH_NOTE, EIGHTH_NOTE,
+  EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE, QUARTER_NOTE, QUARTER_NOTE,
+  QUARTER_NOTE, QUARTER_NOTE, EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE,
+  EIGHTH_NOTE, QUARTER_NOTE, EIGHTH_NOTE, EIGHTH_NOTE, SIXTEENTH_NOTE,  SIXTEENTH_NOTE,  EIGHTH_NOTE, EIGHTH_NOTE,
+  DOT_QUARTER_NOTE, EIGHTH_NOTE, EIGHTH_NOTE, SIXTEENTH_NOTE,  SIXTEENTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE,
+  QUARTER_NOTE, EIGHTH_NOTE, EIGHTH_NOTE, QUARTER_NOTE, QUARTER_NOTE,
+  QUARTER_NOTE, QUARTER_NOTE, HALF_NOTE
+};
 
 const uint16_t noteDurations2[] = {
-    160, 160, 160, 160, 160, 160, 160, 160, 160, 160, 160, 160, 160,
-    160, 160, 160, 160, 160, 160, 160, 160, 160, 160, 160, 160, 160,
-    160, 160, 640, 160, 160, 160, 160, 160, 160, 160, 160, 160, 160,
-    160, 160, 160, 160, 160, 160, 160, 160, 160, 160, 320, 320};
+    EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE,
+    EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE,
+    EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE,
+    EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE,
+    EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE,
+    EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE,
+    EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE,
+    HALF_NOTE,
+    EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE,
+    EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE,
+    EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE,
+    EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE,
+    EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE, EIGHTH_NOTE,
+    QUARTER_NOTE, QUARTER_NOTE, HALF_NOTE};
