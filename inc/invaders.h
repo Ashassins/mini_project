b32881c218a6c97b52c619a5c93364193bece550
Jackson <jrmoffet@gmail.com>
2021-12-01 18:58:25 -0500

    Remove old files

--- inc/invaders.h
+++
@@ -1,7 +0,0 @@
-#ifndef INVADERS_H
-#define INVADERS_H
-
-#include <stdint.h>
-
-
-#endif
--- inc/invaders_dat.h
+++
@@ -1,8 +0,0 @@
-#ifndef INVADERS_H
-#define INVADERS_H
-#include <stdint.h>
-extern const uint16_t invader1[144];
-extern const uint16_t invader2[144];
-extern const uint16_t invader3[144];
-extern const uint16_t ufo[256];
-#endif
