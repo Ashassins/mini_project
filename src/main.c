485c765ca4e15dac613d5d1a230621f35d1a8379
Avanish Subbiah <avanish@pop-os.localdomain>
2021-12-03 23:16:59 -0500

    commented out timer main

--- src/main.c
+++ src/main.c
@@ -19,25 +19,25 @@ static inline void nano_wait(unsigned int n) {
       : "r0", "cc");
 }
 
-// Global counter, modified in timer interrupt
-int glbcnt = 0;
-
-// Timer initializer and interrupt
-void init_tim6() {
-    RCC->APB1ENR |= RCC_APB1ENR_TIM6EN;
-    TIM6->PSC = 1600 - 1;
-    TIM6->ARR = 1000 - 1; // (48000000 / 1600) / 1000 = 30
-    TIM6->DIER |= TIM_DIER_UIE;
-    TIM6->CR1 |= TIM_CR1_CEN;
-    NVIC->ISER[0] |= 1 << TIM6_DAC_IRQn;
-}
-
-void TIM6_DAC_IRQHandler(void) {
-    TIM6->SR &= ~TIM_SR_UIF;
-    glbcnt++;
-    if(glbcnt > 30) glbcnt = 0;
-    update_flags();
-}
+//// Global counter, modified in timer interrupt
+//int glbcnt = 0;
+//
+//// Timer initializer and interrupt
+//void init_tim6() {
+//    RCC->APB1ENR |= RCC_APB1ENR_TIM6EN;
+//    TIM6->PSC = 1600 - 1;
+//    TIM6->ARR = 1000 - 1; // (48000000 / 1600) / 1000 = 30
+//    TIM6->DIER |= TIM_DIER_UIE;
+//    TIM6->CR1 |= TIM_CR1_CEN;
+//    NVIC->ISER[0] |= 1 << TIM6_DAC_IRQn;
+//}
+//
+//void TIM6_DAC_IRQHandler(void) {
+//    TIM6->SR &= ~TIM_SR_UIF;
+//    glbcnt++;
+//    if(glbcnt > 30) glbcnt = 0;
+//    update_flags();
+//}
 
 int main(void) {
   init_nunchuk();
