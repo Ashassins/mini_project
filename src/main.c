cb2dd00941c5bd3fc6be413b866e1e6741b467ad
Avanish Subbiah <avanish@pop-os.localdomain>
2021-12-03 23:52:53 -0500

    Added nunchuk controls to test sprite

--- src/main.c
+++ src/main.c
@@ -69,9 +69,9 @@ int main(void) {
     invader.sprite_data =
         (uint16_t *)(((uint32_t)invader.sprite_data) ^ invader.sprite_swap_key);
     if (flg_mv_right) {
-    	move_sprite(&invader, -5, 0);
+    	move_sprite(&invader, -10, 0);
     } else if (flg_mv_left) {
-    	move_sprite(&invader, 5, 0);
+    	move_sprite(&invader, 10, 0);
     }
     // Draw the invading army
     //draw_invaders();
